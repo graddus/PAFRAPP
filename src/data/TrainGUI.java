28aa5e961a76cfe6fb99ba39fc4d9c95cf0ebe07
Martinj98 <martindaniel98@live.nl>
2017-12-07 10:46:39 +0100

    Add files via upload

--- src/data/TrainGUI.java
+++ src/data/TrainGUI.java
@@ -19,77 +19,78 @@ import design.Train;
 import design.Wagon;
 
 public class TrainGUI {
-		   TrainGUI()
-		   {
-		      //Creating Frame    
-		      Frame fr=new Frame();       
-		      
-		      fr.addWindowListener( new WindowAdapter() {
-		            @Override
-		            public void windowClosing(WindowEvent we) {
-
-		                System.exit(0);
-		            }
-		        } );
-		      //Creating a label
-		      Label lb = new Label("UserId: "); 
-		      
-		      //adding label to the frame
-		      fr.add(lb);           
-		      
-		      //Creating Text Field
-		      TextField t = new TextField();
-		      
-		      //adding text field to the frame
-		      fr.add(t);
-		      
-		      //setting frame size
-		      fr.setSize(1000, 800);  
-		      
-		      //Setting the layout for the Frame
-		      fr.setLayout(new FlowLayout());
-		            
-		      fr.setVisible(true);                
-		   }
-		   public static void main(String args[])
-		   {
-		       TrainGUI ex = new TrainGUI(); 
-		   }
-		}
-
-JComboBox<Integer> jComboBox1 = new JComboBox<Integer>();
-for (Train t:tc.getTrains()){
-jComboBox1.addItem(t.getTrainid());
-}
-
-jComboBox1.addActionListener (new ActionListener () {
-	@Override
-    public void actionPerformed(ActionEvent e) {
-    	Integer selected=(Integer)jComboBox1.getSelectedItem();
-    	Train train=tc.getTrainbyID(selected);
-    	for (Wagon w:train.getWagonlist()){
-			if (w.getClass()==Passengerswagon.class){
-				System.out.println("Passengerswagon");
-				Passengerswagon p1=(Passengerswagon)w;
-				System.out.println("Total seats: "+p1.getSeats());
-			}
-			if (w.getClass()==LiquidCargowagon.class){
-				System.out.println("LiquidCargowagon");
-				LiquidCargowagon p1=(LiquidCargowagon)w;
-				System.out.println("Content in liters: "+p1.getContentliters());
-			}
-			if (w.getClass()==SolidCargowagon.class){
-				System.out.println("SolidCargowagon");
-				SolidCargowagon p1=(SolidCargowagon)w;
-				System.out.println("Content in cubic meters: "+p1.getContentcubic());
-			}
-			if (w.getClass()==Locomotive.class){
-				System.out.println("Locomotive");
-				Locomotive p1=(Locomotive)w;
-				System.out.println("Seat: "+p1.getSeats());
-			}
-		}
-				
 	
-    }
-});
+	static TrainController tc = new TrainController();
+	Frame fr= new Frame();
+	
+	TrainGUI() {
+		System.out.println("StartupTraingui");
+		// Creating Frame
+		fr.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent we) {
+
+				System.exit(0);
+			}
+		});
+		// Creating a label
+		Label lb = new Label("UserId: ");
+		// adding label to the frame
+		fr.add(lb);
+		// Creating Text Field
+		TextField t = new TextField();
+		// adding text field to the frame
+		fr.add(t);
+		// setting frame size
+		fr.setSize(1000, 600);
+
+		// Setting the layout for the Frame
+		fr.setLayout(new FlowLayout());
+
+		fr.setVisible(true);
+	}
+
+	public static void main(String args[]) {
+		System.out.println("Startupmain");
+		TrainGUI ex = new TrainGUI();
+		JComboBox<Integer> jComboBox1 = new JComboBox<Integer>();
+		ex.fr.add(jComboBox1);
+		Label lb = new Label("UserId: ");
+		ex.fr.add(lb);
+		for (Train t : tc.getTrains()) {
+			jComboBox1.addItem(t.getTrainid());
+			System.out.println("Train found");
+		}
+
+		jComboBox1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Integer selected = (Integer) jComboBox1.getSelectedItem();
+				Train train = tc.getTrainbyID(selected);
+				for (Wagon w : train.getWagonlist()) {
+					if (w.getClass() == Passengerswagon.class) {
+						System.out.println("Passengerswagon");
+						Passengerswagon p1 = (Passengerswagon) w;
+						System.out.println("Total seats: " + p1.getSeats());
+					}
+					if (w.getClass() == LiquidCargowagon.class) {
+						System.out.println("LiquidCargowagon");
+						LiquidCargowagon p1 = (LiquidCargowagon) w;
+						System.out.println("Content in liters: " + p1.getContentliters());
+					}
+					if (w.getClass() == SolidCargowagon.class) {
+						System.out.println("SolidCargowagon");
+						SolidCargowagon p1 = (SolidCargowagon) w;
+						System.out.println("Content in cubic meters: " + p1.getContentcubic());
+					}
+					if (w.getClass() == Locomotive.class) {
+						System.out.println("Locomotive");
+						Locomotive p1 = (Locomotive) w;
+						System.out.println("Seat: " + p1.getSeats());
+					}
+				}
+
+			}
+		});
+	}
+}
