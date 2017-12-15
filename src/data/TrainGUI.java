b099120779b0df93cfddd347cd4db0401df6ad62
Martinj98 <martindaniel98@live.nl>
2017-12-15 18:54:07 +0100

    both gui's launch from tc

--- src/data/TrainGUI.java
+++ src/data/TrainGUI.java
@@ -2,57 +2,46 @@ package data;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.UUID;
 import java.awt.*;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.text.LayoutQueue;
 
-import design.LiquidCargowagon;
-import design.Locomotive;
-import design.Passengerswagon;
-import design.SolidCargowagon;
-import design.Train;
-import design.Wagon;
+import design.*;
+
 public class TrainGUI {
 
 	public static JComboBox cbAllTrains;
 	public static JComboBox cbAllWagons;
-	//public static JFrame frame;
+	// public static JFrame frame;
 	public static JPanel contentPane;
-	public static JPanel  paintPane;
+	public static JPanel paintPane;
 	public final static TrainController tc = new TrainController();
-	
-	public TrainGUI() {
+
+	public void displayGUI() {
 		// Setup of GUI
-		JFrame frame= new JFrame();
+		JFrame frame = new JFrame();
 		frame = new JFrame("RichRail1");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		JPanel mainPanel = new JPanel();
 		mainPanel.setLayout(null);
-		
+
 		contentPane = new JPanel();
-		contentPane.setSize(1000,1000);
-		contentPane.setLocation(0,250);
+		contentPane.setSize(1000, 1000);
+		contentPane.setLocation(0, 250);
 		contentPane.setLayout(null);
-		
-		paintPane = new JPanel ();
-		paintPane.setSize(1500,250);
-		paintPane.setLocation(0,0);
+
+		paintPane = new JPanel();
+		paintPane.setSize(1500, 250);
+		paintPane.setLocation(0, 0);
 		paintPane.setLayout(null);
-		
-	
+
 		int x = 15;
-		int y=0;
+		int y = 0;
 		// Adding all the ui Components
 		JLabel lbNewTrain = new JLabel("New Train: ");
 		lbNewTrain.setSize(300, 30);
@@ -61,12 +50,12 @@ public class TrainGUI {
 
 		JTextField tfNewTrain = new JTextField("Train name?");
 		tfNewTrain.setSize(100, 30);
-		tfNewTrain.setLocation(x, y+50);
+		tfNewTrain.setLocation(x, y + 50);
 		contentPane.add(tfNewTrain);
 
 		JButton btnNewTrain = new JButton("Add Train");
 		btnNewTrain.setSize(100, 30);
-		btnNewTrain.setLocation(x, y+100);
+		btnNewTrain.setLocation(x, y + 100);
 		contentPane.add(btnNewTrain);
 		//
 		cbAllTrains = new JComboBox();
@@ -76,17 +65,17 @@ public class TrainGUI {
 
 		JButton btnDeleteTrain = new JButton("Delete Train");
 		btnDeleteTrain.setSize(130, 30);
-		btnDeleteTrain.setLocation(250, y+50);
+		btnDeleteTrain.setLocation(250, y + 50);
 		contentPane.add(btnDeleteTrain);
 
 		cbAllWagons = new JComboBox();
 		cbAllWagons.setSize(150, 30);
-		cbAllWagons.setLocation(250, y+150);
+		cbAllWagons.setLocation(250, y + 150);
 		contentPane.add(cbAllWagons);
 
 		JButton btnDeleteWagon = new JButton("Delete Wagon");
 		btnDeleteWagon.setSize(130, 30);
-		btnDeleteWagon.setLocation(250, y+200);
+		btnDeleteWagon.setLocation(250, y + 200);
 		contentPane.add(btnDeleteWagon);
 
 		// Wagonbuttons
@@ -96,50 +85,50 @@ public class TrainGUI {
 		btnAddWagon1.setLocation(x, y);
 		btnAddWagon1.setForeground(Color.red);
 		contentPane.add(btnAddWagon1);
-		
+
 		JButton btnAddWagon2 = new JButton("+SolidCargo");
 		btnAddWagon2.setSize(130, 30);
-		btnAddWagon2.setLocation(x, y+100);
+		btnAddWagon2.setLocation(x, y + 100);
 		btnAddWagon2.setForeground(Color.blue);
 		contentPane.add(btnAddWagon2);
-		
+
 		JButton btnAddWagon3 = new JButton("+LiquidCargo");
 		btnAddWagon3.setSize(130, 30);
-		btnAddWagon3.setLocation(x, y+200);
+		btnAddWagon3.setLocation(x, y + 200);
 		btnAddWagon3.setForeground(Color.yellow);
 		contentPane.add(btnAddWagon3);
 
 		// extra textfields
 		JLabel lbSeats = new JLabel("Seats: ");
 		lbSeats.setSize(300, 30);
-		lbSeats.setLocation(x, y+50);
+		lbSeats.setLocation(x, y + 50);
 		contentPane.add(lbSeats);
 
 		JTextField tfSeats = new JTextField("20");
 		tfSeats.setSize(80, 30);
-		tfSeats.setLocation(x + 50, y+50);
+		tfSeats.setLocation(x + 50, y + 50);
 		contentPane.add(tfSeats);
 
 		JLabel lbSolid = new JLabel("Content: ");
 		lbSolid.setSize(300, 30);
-		lbSolid.setLocation(x, y+150);
+		lbSolid.setLocation(x, y + 150);
 		contentPane.add(lbSolid);
 
 		JTextField tfSolid = new JTextField("100");
 		tfSolid.setSize(80, 30);
-		tfSolid.setLocation(x + 50, y+150);
+		tfSolid.setLocation(x + 50, y + 150);
 		contentPane.add(tfSolid);
 
 		JLabel lbLiquid = new JLabel("Content: ");
 		lbLiquid.setSize(300, 30);
-		lbLiquid.setLocation(x, y+250);
+		lbLiquid.setLocation(x, y + 250);
 		contentPane.add(lbLiquid);
 
 		JTextField tfLiquid = new JTextField("100");
 		tfLiquid.setSize(80, 30);
-		tfLiquid.setLocation(x + 50, y+250);
+		tfLiquid.setLocation(x + 50, y + 250);
 		contentPane.add(tfLiquid);
-	
+
 		// displaying frame
 		mainPanel.add(paintPane);
 		mainPanel.add(contentPane);
@@ -147,7 +136,7 @@ public class TrainGUI {
 		frame.setSize(800, 600);
 		frame.setLocationByPlatform(true);
 		frame.setVisible(true);
-	
+
 		//// Actionlisteners
 		btnNewTrain.addActionListener(new ActionListener() {
 			@Override
@@ -166,8 +155,8 @@ public class TrainGUI {
 
 		cbAllTrains.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(getSelectedTrain()!= null){
-				loadWagons();
+				if (getSelectedTrain() != null) {
+					loadWagons();
 				}
 				System.out.println("Selected other train!");
 			}
@@ -218,16 +207,19 @@ public class TrainGUI {
 				addwagon("LiquidCargowagon", tfLiquid);
 			}
 		});
+		// AFter setup load Trains and fill comboboxes
+		loadTrains();
 	}
 
-	////Basic Functions
+	//// Basic Functions
 	private void addwagon(String wagontype, JTextField input) {
 		try {
 			int content = Integer.parseInt(input.getText());
-			String id= getRandomid();
-			while(tc.getWagon(id)!= null){
-				id=getRandomid();
-			};
+			String id = tc.getRandomid();
+			while (tc.getWagon(id) != null) {
+				id = tc.getRandomid();
+			}
+			;
 			if (cbAllTrains.getSelectedItem() != null) {
 				if (wagontype.equals("Passengerswagon")) {
 					tc.createPassengerWagon(id, content);
@@ -259,15 +251,15 @@ public class TrainGUI {
 		for (Train t : tc.getTrains()) {
 			cbAllTrains.addItem(t.getTrainid());
 		}
-		if (getSelectedTrain()!=null){
+		if (getSelectedTrain() != null) {
 			loadWagons();
 		}
 	}
 
-	public void loadWagons(){
+	public void loadWagons() {
 		cbAllWagons.removeAllItems();
 		Train selected = getSelectedTrain();
-		for (Wagon w :selected.getWagonlist()){
+		for (Wagon w : selected.getWagonlist()) {
 			if (w.getClass() != Locomotive.class) {
 				cbAllWagons.addItem(w.getWagonid());
 			}
@@ -277,21 +269,21 @@ public class TrainGUI {
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
-
 		}
 
+	}
+
 	public Train getSelectedTrain() {
-		if (cbAllTrains.getSelectedItem()!= null){
+		if (cbAllTrains.getSelectedItem() != null) {
 			return tc.getTrain(cbAllTrains.getSelectedItem().toString());
 		}
 		return null;
 	}
 
 	public void drawTrain(Train train) throws IOException {
-		
+
 		System.out.println("Drawing train");
-		//Draw Locomotive
+		// Draw Locomotive
 		paintPane.removeAll();
 		JLabel locoLabel = new JLabel();
 		locoLabel.setSize(200, 200);
@@ -301,44 +293,42 @@ public class TrainGUI {
 		locoIcon.setImage(locoImg);
 		locoLabel.setIcon(locoIcon);
 		paintPane.add(locoLabel);
-	
-		//Draw all carts
-		int x=0;
-		for (Wagon w: train.getWagonlist()) {
-			if (w.getClass()!= Locomotive.class){
+
+		// Draw all carts
+		int x = 0;
+		for (Wagon w : train.getWagonlist()) {
+			if (w.getClass() != Locomotive.class) {
 				double content = 0;
 				String url = "hetplaatje.png";
-				if (w.getClass() == Passengerswagon.class){
-					Passengerswagon pw =(Passengerswagon) w;
-					content=pw.getSeats();
-					 url = "pass.png";
-				}
-				else if (w.getClass() == SolidCargowagon.class){
-					SolidCargowagon sw =(SolidCargowagon) w;
-					content=sw.getContentcubic();
-					 url = "solid.png";
-				}
-				else{
-					LiquidCargowagon lw =(LiquidCargowagon) w;
-					content=lw.getContentliters();
-					 url = "liquid.png";
+				if (w.getClass() == Passengerswagon.class) {
+					Passengerswagon pw = (Passengerswagon) w;
+					content = pw.getSeats();
+					url = "pass.png";
+				} else if (w.getClass() == SolidCargowagon.class) {
+					SolidCargowagon sw = (SolidCargowagon) w;
+					content = sw.getContentcubic();
+					url = "solid.png";
+				} else {
+					LiquidCargowagon lw = (LiquidCargowagon) w;
+					content = lw.getContentliters();
+					url = "liquid.png";
 				}
 				JLabel idLabel = new JLabel(w.getWagonid());
 				idLabel.setSize(200, 20);
-				idLabel.setLocation(250+x*200, 70);
-				
-				JLabel contentLabel = new JLabel(""+content);
+				idLabel.setLocation(250 + x * 200, 70);
+
+				JLabel contentLabel = new JLabel("" + content);
 				contentLabel.setSize(200, 20);
-				contentLabel.setLocation(210+x*200, 180);
-				
+				contentLabel.setLocation(210 + x * 200, 180);
+
 				JLabel WagonLabel = new JLabel();
 				WagonLabel.setSize(200, 200);
-				WagonLabel.setLocation(170+x*200, 30);
+				WagonLabel.setLocation(170 + x * 200, 30);
 				ImageIcon cartIcon = new ImageIcon();
 				BufferedImage cartImg = ImageIO.read(new File(url));
 				cartIcon.setImage(cartImg);
 				WagonLabel.setIcon(cartIcon);
-				
+
 				paintPane.add(WagonLabel);
 				paintPane.add(idLabel);
 				paintPane.add(contentLabel);
@@ -347,22 +337,5 @@ public class TrainGUI {
 		}
 		paintPane.repaint();
 		System.out.println("Drawing train ending");
-	}
-
-	public String getRandomid(){
-	    Random r = new Random();
-	    String id= "";
-	    String alphabet = "abcdefghijklmnopqrstuvwxyz";
-	        
-	    id+= alphabet.charAt(r.nextInt(alphabet.length()));
-	    id+= alphabet.charAt(r.nextInt(alphabet.length())); 
-	    id+=r.nextInt(999) + 1;
-	    return id;
-	}
-	
-	public static void main(String args[]) {
-		TrainGUI gui = new TrainGUI();
-		gui.loadTrains();
-		 
 	}
 }
